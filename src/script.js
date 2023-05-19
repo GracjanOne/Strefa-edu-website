0ff999082fcf3c9919b98ed22365576af80c6500
unknown <gracjan.zalewski55@gmail.com>
2023-05-19 23:08:16 +0200

    ADD CONTACT CLOSE BUTTON

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 const contact_button = document.getElementById("contact-button");
 const contact_form = document.getElementById("contact-window-container");
-
-contact_button.addEventListener("click", function(event) {
-  event.preventDefault(); 
+const cancel_button = document.getElementById("closeButton");
+contact_button.addEventListener("click", function (event) {
+  event.preventDefault();
   contact_form.style.opacity = "1";
   contact_form.style.visibility = "visible";
   contact_form.style.transition = "0.4s";
   window.scrollBy(0, 350);
+});
+closeButton.addEventListener("click", function (event) {
+  contact_form.style.opacity = "0";
+  contact_form.style.visibility = "hidden";
 });
