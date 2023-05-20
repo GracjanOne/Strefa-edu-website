b7d96ecb8576e3727f582ca5e13843f27c02666b
unknown <gracjan.zalewski55@gmail.com>
2023-05-20 15:16:29 +0200

    ADD FIRST CLICKABLE ASIDE LINK

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 const contact_button = document.getElementById("contact-button");
 const contact_form = document.getElementById("contact-window-container");
 const cancel_button = document.getElementById("closeButton");
+const close_button = document.getElementById("linkCloseButton");
+const link_window = document.getElementsByClassName("links-window")[0];
+
+const zsip = document.getElementById("zsip-link");
+
 contact_button.addEventListener("click", function (event) {
   event.preventDefault();
   contact_form.style.opacity = "1";
@@ -8,7 +13,21 @@ contact_button.addEventListener("click", function (event) {
   contact_form.style.transition = "0.4s";
   window.scrollBy(0, 350);
 });
-closeButton.addEventListener("click", function (event) {
+
+cancel_button.addEventListener("click", function (event) {
   contact_form.style.opacity = "0";
   contact_form.style.visibility = "hidden";
+});
+
+close_button.addEventListener("click", function (event) {
+  link_window.style.opacity = "0";
+  link_window.style.visibility = "hidden";
+});
+
+zsip.addEventListener("click", function(event)
+{
+ event.preventDefault();
+ link_window.style.opacity = "1";
+ link_window.style.visibility = "visible";
+ link_window.style.transition = "0.4s";
 });
