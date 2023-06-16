c31939f648e79852c01375613ae07ee9e8468ed8
unknown <gracjan.zalewski55@gmail.com>
2023-06-16 17:32:39 +0200

    FIX CONSOLE ERRORS ADD NEW FONTS FILES

---
+++ src/bad-resolution.js
@@ -0,0 +1,5 @@
+let width = window.innerWidth;
+if(width<=700)
+{
+  alert("Ta sekcja może być wyświetlana nieodpowiednio na twoim urządzeniu, jeśli chcesz to zmienić użyj komputera lub tabletu.");
+}
--- src/script.js
+++ src/script.js
@@ -3,8 +3,11 @@ const contact_form = document.getElementById("contact-window-container");
 const cancel_button = document.getElementById("closeButton");
 const close_button = document.getElementById("linkCloseButton");
 const link_window = document.getElementsByClassName("links-window")[0];
-const jawor_link_window = document.getElementsByClassName("jawor-links-window")[0];
-const bolkow_link_window = document.getElementsByClassName("bolkow-links-window")[0];
+const jawor_link_window =
+  document.getElementsByClassName("jawor-links-window")[0];
+const bolkow_link_window = document.getElementsByClassName(
+  "bolkow-links-window"
+)[0];
 const zsip = document.getElementById("zsip-link");
 const jawor = document.getElementById("jawor-link");
 const bolkow = document.getElementById("bolkow-link");
@@ -28,40 +31,32 @@ cancel_button.addEventListener("click", function (event) {
 close_button.addEventListener("click", function (event) {
   link_window.style.opacity = "0";
   link_window.style.visibility = "hidden";
-  
 });
 
-zsip.addEventListener("click", function(event)
-{
- event.preventDefault();
- link_window.style.opacity = "1";
- link_window.style.visibility = "visible";
- link_window.style.transition = "0.4s";
- 
+zsip.addEventListener("click", function (event) {
+  event.preventDefault();
+  link_window.style.opacity = "1";
+  link_window.style.visibility = "visible";
+  link_window.style.transition = "0.4s";
 });
-jawor.addEventListener("click", function(event)
-{
+jawor.addEventListener("click", function (event) {
   event.preventDefault();
   jawor_link_window.style.opacity = "1";
   jawor_link_window.style.visibility = "visible";
   jawor_link_window.style.transition = "0.4s";
-  
 });
 jawor_close.addEventListener("click", function (event) {
   jawor_link_window.style.opacity = "0";
   jawor_link_window.style.visibility = "hidden";
-  
 });
-bolkow.addEventListener("click", function(event)
-{
+bolkow.addEventListener("click", function (event) {
   event.preventDefault();
   bolkow_link_window.style.opacity = "1";
   bolkow_link_window.style.visibility = "visible";
   bolkow_link_window.style.transition = "0.4s";
-  
 });
 bolkow_close.addEventListener("click", function (event) {
   bolkow_link_window.style.opacity = "0";
   bolkow_link_window.style.visibility = "hidden";
-  
 });
+
